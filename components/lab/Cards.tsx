16f0c38ff047c80cd8a5ae7aab5e01b48dfd6ca9
Rahul Chaudhari_ <rahulgc545@gmail.com>
2025-10-08 23:11:47 +0530

    done card1

--- components/lab/Cards.tsx
+++ components/lab/Cards.tsx
@@ -1,11 +1,37 @@
+'use client'
+import { useEffect, useState } from 'react'
 import NYC from '../../public/nyc.png'
 import { HeartIcon, TagIcon, PlaneIcon } from 'lucide-react'
+import { animate, press } from 'motion/react'
+
 
 export default function Cards() {
+    const [liked, SetLiked] = useState(false)
+    const [heartColor, setHeartColor] = useState(["text-neutral-100", ""])
+    useEffect(() => {
+
+        if (liked) {
+            setHeartColor(["text-rose-100", "#ffffff70"])
+        } else {
+            setHeartColor(["text-neutral-100", "none"])
+        }
+    }, [liked])
+
+    const handleLike = () => {
+        liked ? SetLiked(false) : SetLiked(true)
+    }
+
+    press(".heart", (element) => {
+        animate(element, { scale: 0.3 }, { type: "spring", stiffness: 1000 })
+
+        return () =>
+            animate(element, { scale: 1 }, { type: "spring", stiffness: 500 })
+    })
+
     return (
         <div>
             <div className="flex flex-row gap-8 " >
-                <div className="w-[22vw] h-[60vh] rounded-4xl p-2  border-1 border-neutral-400 shadow-[0_3px_10px_rgb(0,0,0,0.2)]  " >
+                <div className="w-[22vw] h-[60vh] rounded-4xl p-2    border-1 border-neutral-400 shadow-[0_3px_10px_rgb(0,0,0,0.2)]  " >
                     <div style={{
                         backgroundImage: `url(${NYC.src})`,
                         backgroundSize: 'cover',
@@ -15,7 +41,11 @@ export default function Cards() {
                         <div className='flex flex-col h-full relative z-[999]'>
                             <div className='flex flex-1 justify-end ' >
 
-                                <div className='border-1 border-neutral-100/30 size-12 rounded-full backdrop-blur-md flex justify-center items-center text-neutral-100 font-light'  ><HeartIcon size={18} /> </div>
+                                <span className='border-1 border-neutral-100/30 size-12 rounded-full backdrop-blur-md flex justify-center items-center  font-light'  >
+                                    <button onClick={handleLike} className={`cursor-pointer ${heartColor[0]}  `} >
+                                        <HeartIcon fill={`${heartColor[1]}`} size={18} className='hover:scale-95 heart' />
+                                    </button>
+                                </span>
                             </div>
                             <div className='' >
                                 <div className='text-white text-left  ' >
@@ -36,21 +66,16 @@ export default function Cards() {
                                             <p>JFK</p>
                                         </span>
                                     </span>
-
-
-
                                 </div>
-                                <button className='px-4 py-3 font-semibold text-sm cursor-pointer rounded-full bg-white w-full ' >
+                                <button className='px-4 py-3 hover:bg-neutral-100 hover:scale-[1.005] font-semibold text-sm cursor-pointer rounded-full bg-white w-full ' >
                                     Search Flight
                                 </button>
-
                             </div>
-
                         </div>
-                        <div
-                            className="absolute bottom-0 left-0 w-full h-52 pointer-events-none bg-gradient-to-t from-black/40 to-transparent rounded-t-xl backdrop-brightness-90 "
-                            style={{ WebkitBackdropFilter: 'blur(4px)', backdropFilter: 'blur(4px)' }}
-                        />
+
+                        <div className="absolute bottom-0 left-0 w-full h-52 pointer-events-none">
+                            <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-black/80 via-black/70 to-transparent" />
+                        </div>
                     </div>
                 </div>
                 <div className="w-[25vw] h-[60vh] rounded-4xl p-2  border-1 border-neutral-400 shadow-[0_3px_10px_rgb(0,0,0,0.2)] " >
