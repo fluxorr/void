853bceab4cb20d8344c605b76a39dc8a3f16d40b
Rahul Chaudhari_ <rahulgc545@gmail.com>
2025-11-21 20:56:24 +0530

    add features secitin

--- components/lab/ModernLanding/ModernLanding.tsx
+++ components/lab/ModernLanding/ModernLanding.tsx
@@ -1,6 +1,10 @@
 import Navbar from "./navbar"
 import Hero from "./Hero"
 import TrustedBy from "./trusted-by"
+import Features from "./features"
+import Image from "next/image"
+
+import mountain from "./assets/mountain.png"
 
 const ModernLanding = () => {
     return (
@@ -8,6 +12,10 @@ const ModernLanding = () => {
             <Navbar />
             <Hero />
             <TrustedBy />
+            <Features />
+            <section className="flex justify-center items-center mx-32 my-24" >
+                <Image className="w-[calc(100%-20px)]" alt="mountain view" src={mountain.src} width={1200} height={620} />
+            </section>
         </div>
 
     )
